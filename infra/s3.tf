f96c0c089b113b6785ec9309e7594578d2f766d8
gusoaress <soares.s@aluno.ufabc.edu.br>
2024-05-04 11:25:46 -0300

    Alterações s3

--- infra/s3.tf
+++ infra/s3.tf
@@ -2,31 +2,29 @@
 ##                                 BUCKET S3                                                       ##
 #######################################################################################################
 
-#Criando o bucket de recebimento dos dados
+# Criando o bucket de recebimento dos dados
 resource "aws_s3_bucket" "staging-data-891377294764" {
   bucket = "staging-data-891377294764"
 }
 
-#Upload dos arquivos spotify - fonte https://www.kaggle.com/datasets/tonygordonjr/spotify-dataset-2023?resource=download
+# Upload dos arquivos spotify - fonte https://www.kaggle.com/datasets/tonygordonjr/spotify-dataset-2023?resource=download
 resource "aws_s3_object" "spotify_album" {
-  bucket     = aws_s3_bucket.staging-data-891377294764.bucket
-  key        = "spotify/spotify_album.csv"
-  source     = "../attached-files/spotify-albums_data_2023.csv"
+  bucket = aws_s3_bucket.staging-data-891377294764.bucket
+  key    = "spotify/spotify_album.csv"
+  source = var.spotify_album_source
   depends_on = [aws_s3_bucket.staging-data-891377294764]
 }
 
 resource "aws_s3_object" "spotify_artist" {
-  bucket     = aws_s3_bucket.staging-data-891377294764.bucket
-  key        = "spotify/spotify_artist.csv"
-  source     = "../attached-files/spotify_artist_data_2023.csv"
+  bucket = aws_s3_bucket.staging-data-891377294764.bucket
+  key    = "spotify/spotify_artist.csv"
+  source = var.spotify_artist_source
   depends_on = [aws_s3_bucket.staging-data-891377294764]
 }
 
 resource "aws_s3_object" "spotify_track" {
-  bucket     = aws_s3_bucket.staging-data-891377294764.bucket
-  key        = "spotify/spotify_track.csv"
-  source     = "../attached-files/spotify_tracks_data_2023.csv"
+  bucket = aws_s3_bucket.staging-data-891377294764.bucket
+  key    = "spotify/spotify_track.csv"
+  source = var.spotify_track_source
   depends_on = [aws_s3_bucket.staging-data-891377294764]
 }
-
-
